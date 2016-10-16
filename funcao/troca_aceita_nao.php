f21b0c91b53ddcad17ba2cb7533ccc6db5845d97
Matheus Pellisoli <Matheus Pellisoli@MAtheus-PC>
2016-10-16 18:53:28 -0200

    Sistema de notificação

---
+++ funcao/troca_aceita_nao.php
@@ -0,0 +1,21 @@
+     <?php
+        include '../funcao/conecta.php';
+        
+        $id_troca = $_POST['id_troca'];        
+        $user_i = $_POST['user_i'];
+        $user_d = $_POST['user_d'];  
+         $ProdId_1 = $_POST['ProdId_1'];
+          $ProdId_2 = $_POST['ProdId_2'];            
+
+          
+             $sql_1="UPDATE `produto` SET `ativo`= 1 WHERE `idProduto` = $ProdId_1";
+//executamos a instução SQL
+            mysql_query("$sql_1") or die (mysql_error());
+            
+   
+             $sql_2="UPDATE `produto` SET `ativo`= 1 WHERE `idProduto` = $ProdId_2";
+//executamos a instução SQL
+            mysql_query("$sql_2") or die (mysql_error());        
+            
+            header('Location:../Paginas/Mostra_produtos.php');
+        ?>
