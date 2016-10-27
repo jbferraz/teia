5813c8710263908bc39a308ecdfecbb610972894
Professor <Professor@192.168.200.232>
2016-10-27 20:51:17 -0200

    Titulo da pag editar

--- Paginas/Editar_perfil.php
+++ Paginas/Editar_perfil.php
@@ -26,7 +26,7 @@ window.onload = function() {
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Cadastrar de produto - Projeto teia</title>
+        <title>Editar usuario - Projeto teia</title>
    <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="../bootstrap-3.3.7-dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="../bootstrap-3.3.7-dist/css/Sidenav.css">
