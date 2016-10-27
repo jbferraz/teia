4ddfbf6a4c55e269f1e22cc554980559921ea2d1
Professor <Professor@192.168.200.232>
2016-10-27 18:54:35 -0200

    Edita user

--- Paginas/Editar_perfil.php
+++ Paginas/Editar_perfil.php
@@ -1,27 +1,28 @@
 <?php 
 
 include '../funcao/conecta.php';
-      session_start();
-        if (!isset($_SESSION['Login'])) {  
-            die('<h2>Sessão não iniciada</h2>'); 
+ session_start();
+        if (!isset($_SESSION['Login'])) {
+               
+            die('<h2>Sessão não iniciada</h2>');
+                
         }
-    
    $UserEmail = $_SESSION['Login'];
-    $sql_user = mysql_query("SELECT * FROM `usuario` where `email` = '$UserEmail'");
+    $sql_user = mysql_query("SELECT * FROM `usuario` where `email`='$UserEmail'");
+
         while ($User = mysql_fetch_object($sql_user)) {
             $UserId = $User->idUsuario;
             $UserNome = $User->nome;
-            $UserImg= $User->idImagem;            
-        }      
-     $sql_up_toca= "UPDATE `notificacao` SET `status`= true  WHERE `id_troca` = $id_troca";
-    //executamos a instução SQL
-    //mysql_query("$sql_up_toca") or die (mysql_error())
+            $UserImg= $User->idImagem;
+        }
 ?>
 <script language="javascript" src="../funcao/JavaScript.js"></script>
+
+
 <html>
     <head>
         <meta charset="UTF-8">
-      <title>Editar Perfil - Projeto teia</title>
+        <title>Cadastrar de produto - Projeto teia</title>
    <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="../bootstrap-3.3.7-dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="../bootstrap-3.3.7-dist/css/Sidenav.css">
@@ -41,7 +42,7 @@ include '../funcao/conecta.php';
       <ul class="nav navbar-nav">
           <li><div class="well-sm">
              <div class="navbar-header">
-                 <span  id="menuSitemobile" class="btn-sidbar navbar-toggle" style="float:left;display:none" onclick="toogle()" data-toggle="collapse" data-target="#myNavbar"><span style="color:white" class="glyphicon glyphicon-th-list"></span></span>
+               <span  id="menuSitemobile" class="btn-sidbar navbar-toggle" style="float:left;display:none" onclick="toogle()" data-toggle="collapse" data-target="#myNavbar"><span style="color:white" class="glyphicon glyphicon-th-list"></span></span>
             </div>    
                  <span id="menuSitepc" style="color:white" style="display:block" class="btn-sidbar navbar-collapse" onclick="toogle()"><span style="color:white" class="glyphicon glyphicon-th-list"></span></span><!--SITE NO NENU ABERTO-->
               </div></li>
@@ -58,13 +59,14 @@ include '../funcao/conecta.php';
         <li><a href="Eco_Pontos.php">Ecopontos</a></li>
       </ul>
          <ul class="nav navbar-nav navbar-right">
-            <?php 
+         <?php 
         //teste 
         $sql_not = mysql_query("SELECT COUNT(id_notificacao) as notif FROM view_notificacao where user_i = $UserId and status = 0");
                     while ($Not= mysql_fetch_object($sql_not)) {
                         $Not_num = $Not->notif;
                         }                        
                         if ($Not_num != 0 ) {
+              
                             
                         
        ?>
@@ -72,8 +74,8 @@ include '../funcao/conecta.php';
                  <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-envelope" style="font-size:20px;"></span><span class="badge"><?php echo $Not_num; ?></span></a>
                  <ul class="dropdown-menu" style="min-width:300px;">
         
-         <?php 
-         $sql_not_inf = mysql_query("SELECT * FROM view_notificacao where user_I = $UserId and status = 0");
+        <?php 
+        $sql_not_inf = mysql_query("SELECT * FROM view_notificacao where user_I = $UserId and status = 0");
                  
         while ($Not_inf = mysql_fetch_object($sql_not_inf)){
                     $id_not = $Not_inf->id_notificacao;
@@ -81,23 +83,64 @@ include '../funcao/conecta.php';
                   $user_interece = $Not_inf->nome;  
                   $mensagem =  $Not_inf->mensagem;
                   $tipo = $Not_inf->tipo;
-          
-       ?> 
+       ?>   
                <li><div class="col-sm-12" style="width:100%;padding:2px; border-bottom:0.5px solid black; margin-bottom: 5px">
                          <div>
                          <div class="col-sm-4" >
                            <span class="glyphicon glyphicon-tags" style="font-size:50px;margin:auto; margin-top:4px;">   
                          </div>                         
                                  <div class="col-sm-8 text-left" style="margin-top:4px;">
-                            <?php echo "$user_interece fez uma proposta de troca para você"; ?>
+                            <?php echo "$user_interece $mensagem"; ?>
                                      
                          </div>
+                             <?php
+                                    if ($tipo == 0) {
+                                        
+                                   
+                             ?>
                                   <form class="form-horizontal"  method="post"  enctype="multipart/form-data" action="../Paginas/Trocas.php">
                                       <input type="submit" class="btn btn-default" style="float: right; margin-right: 10px; margin-bottom: 5px" value="detalhes da oferta">
-                                 <input type="hidden" name="id_troca" id="idtroca" value="<?php echo $id_troca; ?>">                                  
+                                 <input type="hidden" name="id_troca" id="idtroca" value="<?php echo $id_troca; ?>">  
+                                 <input type="hidden" name="id_not" id="idtroca" value="<?php echo $id_not; ?>"> 
                                  </form>
                                      
-                         
+                         <?php
+                                    }
+                                        
+                                   
+                             ?>
+                              <?php
+                                    if ($tipo == 1) {
+                                        
+                                   
+                             ?>
+                             <form class="form-horizontal"  method="post"  enctype="multipart/form-data" action="../funcao/notificacao.php  ">
+                                      <input type="submit" class="btn btn-default" style="float: right; margin-right: 10px; margin-bottom: 5px" value="Ok">
+                                 <input type="hidden" name="id_troca" id="idtroca" value="<?php echo $id_troca; ?>">  
+                                 <input type="hidden" name="id_not" id="idtroca" value="<?php echo $id_not; ?>"> 
+                                 </form>
+                                     
+                         <?php
+                                    }
+                                        
+                                   
+                             ?>
+                               <?php
+                                    if ($tipo == 2) {
+                                        
+                                   
+                             ?>
+                             <form class="form-horizontal"  method="post"  enctype="multipart/form-data" action="../Paginas/troca_aceita.php ">
+                                 <input type="submit" class="btn btn-default" style="float: right; margin-right: 10px; margin-bottom: 5px" value="detalhes da oferta">
+                                 <input type="hidden" name="id_troca" id="idtroca" value="<?php echo $id_troca; ?>">  
+                                 <input type="hidden" name="id_not" id="idtroca" value="<?php echo $id_not; ?>"> 
+                                 </form>
+                                     
+                         <?php
+                                    }
+                                        
+                                   
+                             ?>
                          </div></div></li> 
                          
                          
@@ -128,25 +171,25 @@ include '../funcao/conecta.php';
             ?>     
                  
              
-        </li> 
+        </li>
         <li><a href="../funcao/sair.php"><span class="glyphicon glyphicon-log-out"></span> Sair</a></li>
         
     </div>
   </div>
 </nav>
     <!--Termina o menu -->
-    <div class="jumbotron text-center" style="margin-top:40px;background:#FFF" align="center">
-        <img  style="margin:0 auto"class="img-responsive" src="Listar.php?codigo=0" alt="Chania">
-   <hr style="width:75%">
+    <div class="jumbotron text-center" style="background:white; margin-top:40px;" align="center">
+    <img class="img-responsive" src="../Imagens/logo.png" alt="Chania">
+    <hr style="width:75%">
 </div> 
   <!--Menu central do usuario menu-sidnav-->
   <div id="mySidenav-tt" class="sidenav-tt">
       <div id="mySidenav" class="sidenav" align="center">
-       <ul align="center" style="list-style:none;color:white;padding-left:5px"> 
+          <ul align="center" style="list-style:none;color:white;padding-left:5px"> 
       <li><img class="img-responsive " src="<?php echo"Listar.php?codigo=$UserImg"; ?>" alt="Chania" style="min-height:150px;max-height:200px;margin:auto;"></li>
       <li><?php echo $UserNome; ?></li>
       <hr style="width:75%;margin:10px auto">
-      <li style="padding:10px">
+        <li style="padding:10px">
           <h4>Rank</h4>
           
           <?php         
@@ -154,7 +197,7 @@ include '../funcao/conecta.php';
         while ($User = mysql_fetch_object($sql_user)) {        
             $Usernota= $User->media_poderada;
             $Usernota= ceil($Usernota);
-            
+           
             if (!empty($Usernota)) {
                   
             
@@ -189,11 +232,11 @@ include '../funcao/conecta.php';
         </form>
  
       </li>
-      <li><div style="border:1px solid white;border-radius:10px; width:90%;margin:auto;margin-bottom:10px;"><a href="Mostra_produtos.php?pag=1" style="margin:auto;font-size:18px;"><span class="glyphicon glyphicon-shopping-cart"></span>Produtos</a></div></li>
+      <li><div style="border:1px solid white;border-radius:10px; width:90%;margin:auto;margin-bottom:10px;"><a href="Mostra_produtos.php" style="margin:auto;font-size:18px;"><span class="glyphicon glyphicon-shopping-cart"></span>Produtos</a></div></li>
       <li><div style="border:1px solid white;border-radius:10px; width:90%;margin:auto;margin-bottom:10px;;"><a href="Cadastrar_produto.php" style="margin:auto;font-size:18px;"><span class="glyphicon glyphicon-plus"></span> Adicionar Produto</a></div></li>
-      <li><div style="border:1px solid white;border-radius:10px; width:90%;margin:auto;margin-bottom:10px;;"><a href="Meus_produtos.php?pag=1" style="margin:auto;font-size:18px;"><span class="glyphicon glyphicon-folder-open"></span> Meus Produto</a></div></li>
-      <li><div style="border:1px solid white;border-radius:10px; width:90%;margin:auto;margin-bottom:10px;;"><a href="Historico_Oferta.php?pag=1" style="margin:auto;font-size:18px;"><span class="glyphicon glyphicon-folder-open"></span> Histórico de ofertas</a></div></li>
-       </ul> 
+      <li><div style="border:1px solid white;border-radius:10px; width:90%;margin:auto;margin-bottom:10px;;"><a href="Meus_produtos.php" style="margin:auto;font-size:18px;"><span class="glyphicon glyphicon-folder-open"></span> Meus Produto</a></div></li>
+      <li><div style="border:1px solid white;border-radius:10px; width:90%;margin:auto;margin-bottom:10px;;"><a href="Historico_Oferta.php" style="margin:auto;font-size:18px;"><span class="glyphicon glyphicon-folder-open"></span> Histórico de ofertas</a></div></li>
+          </ul> 
 </div>
       <div class="btn-sidbar-tt">
           <button class="btn-sidbar-tt" onclick="toogle()" ></button>
@@ -201,7 +244,7 @@ include '../funcao/conecta.php';
   </div>
 
 
-    <!-- fim menu-->    
+    <!-- fim menu--> 
     <div class="container">
     <div class="col-sm-4">
        
@@ -226,18 +269,18 @@ include '../funcao/conecta.php';
   
   <form class="form-horizontal"  method="post" action="../funcao/editarProd.php" enctype="multipart/form-data">
     <div class="form-group">
-        <input name="ProdNome" type="text" class="form-control" id="email" value="<?php echo $nome; ?>">
+        <input name="UserNome" type="text" class="form-control" id="email" value="<?php echo $nome; ?>">
     </div>
     <div class="form-group">
-        <input name="ProdDesc" type="text" class="form-control" id="senha" value="<?php echo $email; ?>" >
+        <input name="UserEmail" type="text" class="form-control" id="senha" value="<?php echo $email; ?>" >
     </div>
      <div class="form-group">
          
      </div>
   <div class="form-group">     
 <div class="form-group">
-      <select class="form-control" id="sel1" name="ProdCategoria">
-        <option value="" disabled selected>Selecione a categoria</option>
+      <select class="form-control" id="sel1" name="UserCategoria">
+       
          <?php
                     $sql = mysql_query("SELECT * FROM `usuariocategoria` WHERE `idUsuario`= $UserId");
                     while ($CategU = mysql_fetch_object($sql)) {
@@ -253,7 +296,7 @@ include '../funcao/conecta.php';
                         }  else {
                             echo "<option value='$Categ_id'>$Categ_nome</option> ";
                         }
-                        echo "<option value='$Categ_id'>$Categ_nome</option> ";
+                       // echo "<option value='$Categ_id'>$Categ_nome</option> ";
                     }
                     ?>
       </select>
